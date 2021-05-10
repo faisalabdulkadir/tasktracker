419aabf056d808030c61419b5b067b18ae51e69c
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-05-10 15:34:34 +0100

    created addtask component to handle form input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -34,9 +36,19 @@ function App() {
     setTasks(tasks.map((task) => task.id === id ?
     {...task, reminder: !task.reminder} : task ));
   }
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {id, ...task};
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+     {showAddTask && <AddTask onAdd={addTask} />}
+
       {tasks.length > 0 ?
         <Tasks
           tasks={tasks}
